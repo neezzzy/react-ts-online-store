1bb73abf75a057d9ab0cfb8aecbe6a915ab42972
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-07-25 22:53:47 -0700

    initial commit

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+
+type StoreItemProps = {
+  id: number;
+  make: string;
+  price: number;
+  model: string;
+  imgUrl: string;
+};
+import { formatCurrency } from "../utils/formatCurrency";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+
+export default function StoreItem({
+  id,
+  make,
+  price,
+  model,
+  imgUrl,
+}: StoreItemProps) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200"
+        style={{ objectFit: "cover" }}
+      ></Card.Img>
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{make}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add to Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: "0.5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: "0.5rem" }}
+              >
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+              </div>
+              <div>
+                <Button
+                  onClick={() => removeFromCart(id)}
+                  variant="danger"
+                  size="sm"
+                >
+                  Remove
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
